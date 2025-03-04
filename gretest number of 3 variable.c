c1a3ecbd7b0efd63f19327e390125371c3bcc5ce
Preeti <preeti159080@gmail.com>
2025-03-04 14:33:25 +0530

    Add files via upload

---
+++ gretest number of 3 variable.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,num3,gretest;
+	
+	printf("Enter the first number=");
+	scanf("%d",&num1);
+	printf("Enter the second  number=");
+	scanf("%d",&num2);
+	printf("Enter the third number=");
+	scanf("%d",&num3);
+	if(num1<=num2 && num1<=num3)
+	{ 
+	printf("%d is gretest number 1");
+	}
+	else if( num2<=num1 && num2<=num3)
+	{
+	 printf("%d is gretest number 2");
+	}
+	else
+	{ 
+	printf("%d is gretest number 3");
+	}
+	return 0;
+}
+
