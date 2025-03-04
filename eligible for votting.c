08c53cd511db7214d288c23b169d48c19b2e72d1
Preeti <preeti159080@gmail.com>
2025-03-04 14:32:23 +0530

    Add files via upload

---
+++ eligible for votting.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("enter age:");
+	scanf("%d",&age);
+	if(age>=18)
+	printf("eligible for votting");
+	else
+	printf("not eligible for votting");
+	return 0;
+}
